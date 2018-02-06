605790fcc9cd6cb6b357c8d5b724a79a965e8ef2
Marat Shagiev <m_js@bk.ru>
2018-02-06 18:15:02 +0300

    Begin to construct function to add the export sets. Make function to add Title Div. Function to add Form don't work. Add label 'minus' that been used to delete the export set.

--- js/main.js
+++ js/main.js
@@ -40,6 +40,14 @@
       }, 100);
     });
     $('.title-bg').click(function (e) {
+      if (this.getAttribute('id') == 'add_new_set') {
+        var newSet = new AddNewExportSet();
+        newSet.addNewSet('Test', this.parentElement);
+        setTimeout(function () {
+          fitWindowToContent();
+        }, 100);
+        return;
+      }
       var formName = e.target.nextElementSibling.getAttribute('id').slice(5);
       $('#exp_content_' + formName).toggleClass('hiddenElem');
       setTimeout(function () {
@@ -117,5 +125,133 @@
   }
   function reloadPanel() {
     location.reload();
+  }
+
+  /**
+   * @constructor
+   * */
+  function AddNewExportSet() {
+    this.devMessage = function () {
+      alert('Adding a new set in develope');
+    };
+    this.addNewSet = function (setName, elem) {
+      var elemTitle = _addSetTitle(setName, elem);
+      _addForm(setName, elemTitle);
+
+      /**
+       * @private
+       * */
+      function _addForm(setName, elem) {
+        var formContent = document.createElement('form')/*,
+         divExpCont = document.createElement('div'),
+         divFirstLine = document.createElement('div'),
+         divCompress = document.createElement('div'),
+         divAntialias = document.createElement('div'),
+         divArtbs = document.createElement('div'),
+         divPostfix = document.createElement('div'),
+         divBtns = document.createElement('div')*/;
+        formContent.className = 'form_' + setName.toLowerCase();
+        formContent.innerHTML = '      <div id="exp_content_" + setName.toLowerCase() class="exp-content hiddenElem">\n' +
+                                '        <div class="line">\n' +
+                                '          <label for="sel_color_model_" + setName.toLowerCase()>Color Model:</label>\n' +
+                                '          <select id="sel_color_model_" + setName.toLowerCase() class="hostTextarea">\n' +
+                                '            <option>RGB</option>\n' +
+                                '            <option>CMYK</option>\n' +
+                                '            <option>Grayscale</option>\n' +
+                                '          </select>\n' +
+                                '          <label for="nmb_quality_" + setName.toLowerCase()>Quality:</label>\n' +
+                                '          <input type="number" id="nmb_quality_" + setName.toLowerCase() class="hostTextarea" min="1" max="10" step="1"\n' +
+                                '                 value="6">\n' +
+                                '          <label for="sel_resolution_" + setName.toLowerCase()>Resolution:</label>\n' +
+                                '          <select id="sel_resolution_" + setName.toLowerCase() class="hostTextarea">\n' +
+                                '            <option>72</option>\n' +
+                                '            <option>150</option>\n' +
+                                '            <option>200</option>\n' +
+                                '            <option>250</option>\n' +
+                                '            <option>300</option>\n' +
+                                '            <option>350</option>\n' +
+                                '            <option>400</option>\n' +
+                                '            <option>450</option>\n' +
+                                '            <option>500</option>\n' +
+                                '            <option>550</option>\n' +
+                                '          </select>\n' +
+                                '        </div>\n' +
+                                '        <div id="compress_method_" + setName.toLowerCase() class="line">\n' +
+                                '          <label for="sel_compress_method_" + setName.toLowerCase()>Compression Method:</label>\n' +
+                                '          <select id="sel_compress_method_" + setName.toLowerCase() class="hostTextarea">\n' +
+                                '            <option>Baseline (Standard)</option>\n' +
+                                '            <option>Baseline Optimized</option>\n' +
+                                '            <option>Progressive</option>\n' +
+                                '          </select>\n' +
+                                '          <label for="sel_scans_" + setName.toLowerCase()>Scans:</label>\n' +
+                                '          <select id="sel_scans_" + setName.toLowerCase() class="hostTextarea">\n' +
+                                '            <option>03</option>\n' +
+                                '            <option>04</option>\n' +
+                                '            <option>05</option>\n' +
+                                '          </select>\n' +
+                                '        </div>\n' +
+                                '        <div id="antialias_" + setName.toLowerCase() class="line">\n' +
+                                '          <label for="sel_antialias_" + setName.toLowerCase()>Anti-aliasing:</label>\n' +
+                                '          <select id="sel_antialias_" + setName.toLowerCase() class="hostTextarea">\n' +
+                                '            <option>None</option>\n' +
+                                '            <option>Art Optimized (Supersampling)</option>\n' +
+                                '            <option>Text Optimized (Hinted)</option>\n' +
+                                '          </select>\n' +
+                                '        </div>\n' +
+                                '        <div id="artbs_" + setName.toLowerCase() class="line">\n' +
+                                '          <input type="checkbox" value="Use artboards" id="ch_use_artbs_" + setName.toLowerCase()>\n' +
+                                '          <label for="ch_use_artbs_" + setName.toLowerCase()>Use Artboards</label>\n' +
+                                '          <input type="radio" id="rad_artbs_all_" + setName.toLowerCase() name="choose_artbs" checked>\n' +
+                                '          <label for="rad_artbs_all_" + setName.toLowerCase()>All</label>\n' +
+                                '          <input type="radio" id="rad_artbs_range_" + setName.toLowerCase() name="choose_artbs">\n' +
+                                '          <label for="rad_artbs_range_" + setName.toLowerCase()>Range</label>\n' +
+                                '          <label for="txt_artbs_range_" + setName.toLowerCase()></label>\n' +
+                                '          <input type="text" id="txt_artbs_range_" + setName.toLowerCase() class="hostTextarea" value="2-3">\n' +
+                                '        </div>\n' +
+                                '        <div id="postfix_" + setName.toLowerCase() class="line">\n' +
+                                '          <label for="txt_postfix_" + setName.toLowerCase()>Postfix Label:</label>\n' +
+                                '          <input type="text" id="txt_postfix_" + setName.toLowerCase() class="hostTextarea" value=""\n' +
+                                '                 placeholder="" width="200px">\n' +
+                                '        </div>\n' +
+                                '        <div id="btns_" + setName.toLowerCase() class="line">\n' +
+                                '          <input type="button" id="btn_defaults_" + setName.toLowerCase() class="hostButton" value="Defaults"/>\n' +
+                                '        </div>\n' +
+                                '      </div>';
+        // elem.insertBefore(formContent, elem.lastElementChild);
+      }
+      /**
+       * @private
+       * */
+      function _addSetTitle(setName, elem) {
+        var inputSetName = prompt('Input the set name: ', setName);
+
+        var divTitleBg = document.createElement('div');
+        var divExpTitle = document.createElement('div');
+        var labelName = document.createElement('label');
+        var labelDel = document.createElement('label');
+        var inputBtnExport = document.createElement('input');
+
+        divTitleBg.className = 'title-bg';
+        divExpTitle.className = 'exp-title';
+        labelName.className = 'setName';
+        labelDel.className = 'setName';
+        labelName.innerHTML = inputSetName;
+        labelDel.innerHTML = ' (–)';
+
+        inputBtnExport.className = 'hostButton btn-export';
+        inputBtnExport.setAttribute('type', 'button');
+        inputBtnExport.setAttribute('id', 'btn_export_' + inputSetName.toLowerCase());
+        inputBtnExport.setAttribute('value', 'Export');
+
+        divExpTitle.appendChild(labelName);
+        divExpTitle.appendChild(labelDel);
+        divTitleBg.appendChild(divExpTitle);
+        divTitleBg.appendChild(inputBtnExport);
+
+         elem.insertBefore(divTitleBg, elem.lastElementChild);
+         return divTitleBg;
+      }
+    };
+
   }
 }());
